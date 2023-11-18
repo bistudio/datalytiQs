25d05704c078b30b6236d4b267463d40b2515ee2
pyguypy <basswrks@hotmail.com>
2023-11-18 16:50:14 +0000

    class definition

---
+++ snippets/python_snippets/basics/array/py/main.py
@@ -0,0 +1,36 @@
+## testing python array & lists
+
+myarray = [1,2,3,4,5,6,7,8,9,10]
+
+print(f'the array or list is ' + str(myarray))
+
+### use length to check the size of the array
+
+print(f'the size of array or list is ' + str(len(myarray)))
+
+### use append to add an item to the end of the array
+
+myarray.append(11)
+item = myarray[len(myarray)-1]
+print(f'{item}' + ' was appended to the list or array')
+print(myarray)
+
+### use insert to add an item to the array at a specific index
+
+myarray.insert(0,0)
+print(myarray)
+
+### use pop to remove an item from the end of the array
+
+myarray.pop(2)
+print(myarray)
+
+### use remove to remove an item from the array at a specific index
+
+myarray.remove(0)
+print(myarray)
+
+### use del to remove an item from the array at a specific index
+
+del myarray[0]
+print(myarray)
